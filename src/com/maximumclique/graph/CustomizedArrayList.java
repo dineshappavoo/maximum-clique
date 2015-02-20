c035c90dc7621599d413705f529a4a573dea8b3f
dinesgappavoo <dinesha.cit@gmail.com>
2015-02-20 02:18:29 -0600

    Updated vertex cover for algo and custom array

---
+++ src/com/maximumclique/graph/CustomizedArrayList.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.maximumclique.graph;
+
+import java.util.Arrays;
+
+/**
+ * @author Dany
+ *
+ */
+public class CustomizedArrayList {
+
+	
+	private int[] array;
+	private int size;
+	public CustomizedArrayList(int size)
+	{
+		this.size = size;
+		array = new int[size];
+		Arrays.fill(array, 0);
+	}
+	
+	/**
+	 * Array function to find whether value already exists
+	 * @param value
+	 * @return
+	 */
+	public boolean contains(int value)
+	{
+		for(int i=0;i<size;i++)
+		{
+			if(array[i]==value)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Array method to add element to a specific index
+	 * @param index
+	 * @param value
+	 * @return
+	 */
+	public boolean add(int index, int value)
+	{
+		if(array[index]!=0)
+		{
+			return false;
+		}
+		array[index] = value;
+		return true;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/maximumclique/graph/MinimumVertexCover.java
+++ src/com/maximumclique/graph/MinimumVertexCover.java
@@ -29,6 +29,13 @@ import java.util.Set;
  *  - Vertex cover vertices includes,
  *     - Vertices which are participating in the matching and also connected to any of the 
  *       free vertices(both sides)
+ *          - iterate over free vertices from left side. 
+ *          - for vertex u which is free on  left side
+ *              - if any marked vertex connects to u, skip to next free vertex on left side
+ *              - else, check the set of unmarked vertices which participate in the matching
+ *                   - mark the first of these vertices which connects to u.
+ *          - include all of the marked vertices in the vertex cover  
+ *          - do the same for the right side.
  *     - Vertices which are not connected to the free vertices (From any one side)  
  */
 public class MinimumVertexCover {
@@ -57,7 +64,7 @@ public class MinimumVertexCover {
 		//int result=minVertCover.findMaxMatching("/Users/Dany/Documents/FALL-2013-COURSES/Imp_Data_structures/workspace/MaximumClique/src/com/maximumclique/input/matching_input_5.txt");
 		minVertCover.getVertexCoverTest();
 	}
-	
+
 	public void getVertexCoverTest() throws FileNotFoundException
 	{
 		Graph graph = constructGraph("/Users/Dany/Documents/FALL-2013-COURSES/Imp_Data_structures/workspace/MaximumClique/src/com/maximumclique/input/matching_input_5.txt");
@@ -76,7 +83,7 @@ public class MinimumVertexCover {
 		Scanner scanner=new Scanner(infile);
 		leftVertices=scanner.nextInt();
 		rightVertices=scanner.nextInt();
-		
+
 		noOfEdges=scanner.nextInt();
 		biGraph=new Graph(leftVertices+rightVertices+1);
 		for(int i=0;i<noOfEdges;i++)
@@ -86,19 +93,19 @@ public class MinimumVertexCover {
 			biGraph.addEdge(u,leftVertices+v);// To distinct the vertex ID's 'leftVertice+v'
 			biGraph.addEdge(leftVertices+v, u);
 		}
-		
+
 		//Shuffle the adjacency list
 		ArrayList<Integer>[] adjList = biGraph.getAdjacencyList();
-		
+
 		//Shuffle the vertices
 		//GraphDNA.shuffle(adjList);
-		
+
 		//Shuffle each adjacency list
 		for(ArrayList<Integer> list : adjList)
 		{
 			Collections.shuffle(list);
 		}
-		
+
 		return biGraph;
 	}
 
@@ -111,9 +118,10 @@ public class MinimumVertexCover {
 		int result=findMaxMatching(graph);
 		System.out.println("\nTotal matching : "+result);
 		identifyFreeVertices();
-		findMinimumVertexCover();
-		printDetailedGraphInformation();		
-		
+		findMinimumVertexCover1();
+		printDetailedGraphInformation();
+		printMatching();
+
 		return new BiPartiteGraph(leftVertexCoverVertices, rightVertexCoverVertices);
 	}
 
@@ -130,7 +138,7 @@ public class MinimumVertexCover {
 
 		for(int i : rightFreeVertices)
 		{
-			System.out.println("Right Free : "+i);
+			System.out.println("Right Free : "+(i-leftVertices));
 		}
 		System.out.println("-------------------------------");
 		System.out.println("   VERTEX COVER   ");
@@ -142,9 +150,9 @@ public class MinimumVertexCover {
 
 		for(int i : rightVertexCoverVertices)
 		{
-			System.out.println("Right Vertex Cover : "+i);
+			System.out.println("Right Vertex Cover : "+(i-leftVertices));
 		}		
-		
+
 		System.out.print("Vertex Cover List : [ ");
 		for(int i : leftVertexCoverVertices)
 		{
@@ -158,7 +166,6 @@ public class MinimumVertexCover {
 		System.out.print("]");
 		System.out.println("\n===============================");
 
-		
 	}
 
 	/**
@@ -169,7 +176,8 @@ public class MinimumVertexCover {
 	{
 		for(int i=1;i<=leftVertices;i++)
 		{
-			if(Pair[i]!=0)
+
+			if(Pair[i]!=0)	//Vertices part of matching
 			{
 				ArrayList<Integer> adjacencyList = biGraph.getOutEdges(i);
 				for(int n : adjacencyList)
@@ -177,13 +185,14 @@ public class MinimumVertexCover {
 					if(rightFreeVertices.contains(n))
 					{
 						leftVertexCoverVertices.add(i);
+						break;
 					}
 					else // In both cases we are adding because any one side can be added to vertex cover list
 					{
 						leftVertexCoverVertices.add(i);
 					}
 				}
-				System.out.println("Left  : "+(i)+"  "+(Pair[i]-leftVertices));
+				//System.out.println("Left  : "+(i)+"  "+(Pair[i]-leftVertices));
 			}
 		}
 		System.out.println();
@@ -194,21 +203,96 @@ public class MinimumVertexCover {
 				ArrayList<Integer> adjacencyList = biGraph.getOutEdges(j);
 				for(int n : adjacencyList)
 				{
-					if(leftFreeVertices.contains(n))
+					if(leftFreeVertices.contains(n)) {
 						rightVertexCoverVertices.add(j);
+						break;
+					}
 				}
-				System.out.println("Right : "+(j-leftVertices)+"  "+Pair[j]);
+				//if(!rightVertexCoverVertices.contains(j))
+				//rightVertexCoverVertices.add(j);
+				//System.out.println("Right : "+(j-leftVertices)+"  "+Pair[j]);
+			}
+		}
+	}
+
+	public void findMinimumVertexCover1()
+	{
+		//Left Free
+		for(int leftFreeNode : leftFreeVertices)
+		{
+			if(!leftVertexCoverVertices.contains(leftFreeNode))
+			{
+				ArrayList<Integer> adjList = biGraph.getOutEdges(leftFreeNode);
+				for(int n : adjList)
+				{
+					if(!rightVertexCoverVertices.contains(n))
+					{
+						if(Pair[n]!=0)
+						{
+							rightVertexCoverVertices.add(n);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+
+		//Right Free
+		for(int rightFreeNode : rightFreeVertices)
+		{
+			if(!rightVertexCoverVertices.contains(rightFreeNode))
+			{
+				ArrayList<Integer> adjList = biGraph.getOutEdges(rightFreeNode);
+				for(int n : adjList)
+				{
+					if(!leftVertexCoverVertices.contains(n))
+					{
+						if(Pair[n]!=0)
+						{
+							leftVertexCoverVertices.add(n);
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		//Matching elements from any one side
+		for(int i=1;i<=leftVertices;i++)
+		{
+			if(Pair[i]!=0)
+			{
+				if(!leftVertexCoverVertices.contains(i))
+				{
+					leftVertexCoverVertices.add(i);
+				}
 			}
 		}
 	}
 
 
+	public void printMatching()
+	{
+		String pair="";
+		for(int i=1;i<=leftVertices;i++)
+		{
+			if(Pair[i]==0)
+				pair="Nil";
+			else
+				pair=String.valueOf((Pair[i]-leftVertices));
+			System.out.print("("+i+","+(pair)+")");
+		}
+	}
 	/**
 	 * Method to find the free vertices after the bipartite matching
 	 * 
 	 */
 	public void identifyFreeVertices()
 	{
+		System.out.println("left No"+leftVertices);
+		System.out.println("right No"+rightVertices);
+
 		for(int i=1;i<=leftVertices;i++)
 		{
 			if(Pair[i]==0)
